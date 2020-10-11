6c2a87328d25f693fa818989fda1a8c8655ef740
steven <undefined>
2020-10-11 19:31:27 +0100

    fix init script with absolute path

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
+ln -s "$(pwd)" ~/dot-files
 
-ln -s zsh/oh-my-zsh ~/.oh-my-zsh
-ln -s zsh/zshrc ~/.zshrc
-ln -s zsh/p10k.zsh ~/.p10k.zsh
-
+ln -s ~/dot-files/zsh/oh-my-zsh ~/.oh-my-zsh
+ln -s ~/dot-files/zsh/zshrc ~/.zshrc
+ln -s ~/dot-files/zsh/p10k.zsh ~/.p10k.zsh
