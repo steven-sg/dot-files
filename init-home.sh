e19a5429b908e4a4017d2f14eec9ade2260b93b6
steven <undefined>
2020-12-15 11:52:24 +0000

    Add init script for home desktop

---
+++ init-home.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+ln -s "$(pwd)" ~/dotfiles
+
+mkdir -p ~/.config/git/
+
+ln -s ~/dotfiles/zsh/oh-my-zsh ~/.oh-my-zsh
+ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
+ln -s ~/dotfiles/git/.gitconfig ~/.config/git/config
+ln -s ~/dotfiles/skhdrc ~/.skhdrc
+ln -s ~/dotfiles/yabairc ~/.yabairc
