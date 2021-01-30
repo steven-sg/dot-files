5e0249dcfb6dd8fcc975ce8bfdec4af7c0566d7c
steven <undefined>
2021-01-30 17:09:56 +0000

    Add virtual cam info comment

--- scripts/mac/virtual_cam_discord.sh
+++ scripts/mac/virtual_cam_discord.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 
+# For more information read: https://github.com/johnboiles/obs-mac-virtualcam/wiki/Compatibility#apps-dont-allow-dal-plugins
+
 if [ ! -d /Applications/Discord.app ]; then
     echo "ABORTING: Discord.app is not found."
     exit
